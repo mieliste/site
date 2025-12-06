a3ed223c4ef3bdf5a6c5309eff27632080e883ab
jeremy <jeremy.kusper@etu.unistra.fr>
2025-12-06 19:06:50 +0100

    icone 3D reseaux en cours

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ menuIcon.addEventListener('click',()=>{
 nav.classList.toggle('mobile-menu')
 console.log("salut")
 })
+
+/* Image insta  */
+
+const img = document.getElementById('thumb');
+img.addEventListener('mouseenter', () => {
+  img.src = img.dataset.gif;
+});
+img.addEventListener('mouseleave', () => {
+  img.src = img.dataset.still;
+});
+
+/* Pour écrans tactiles : lancer au tap et basculer */
+
+/*img.addEventListener('click', () => {
+  img.src = img.src === img.dataset.gif ? img.dataset.still : img.dataset.gif;
+
+
+});
+*/
