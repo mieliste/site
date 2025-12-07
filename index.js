ab8d6328f6de2fcb7555e4b6676ebed578536bde
jeremy <jeremy.kusper@etu.unistra.fr>
2025-12-07 22:20:52 +0100

    accueil en cours

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ menuIcon.addEventListener('click',()=>{
 nav.classList.toggle('mobile-menu')
 console.log("salut")
 })
+
+const ruche = document.querySelector(".ruche-image");
+
+
+const btnRetour = document.getElementById('btn-retour');
+const vueInterieure = document.getElementById('interieur-ruche');
+
+// Quand on clique sur la ruche
+ruche.addEventListener('click', () => {
+    vueInterieure.classList.add('active'); // Le CSS gère l'apparition
+});
+
+// Quand on clique sur "Sortir"
+btnRetour.addEventListener('click', () => {
+    vueInterieure.classList.remove('active'); // Le CSS gère la disparition
+});
